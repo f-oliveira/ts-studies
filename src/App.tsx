1fda9c99c936fc84a04ce88f02e89f3bb1cc03fa
Felipe N. Oliveira <felipe.noliveira19@gmail.com>
2023-02-28 20:17:46 -0300

    feat: Create task form.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className="App">
-      
+      <TaskForm/>
+      <TaskList/>
     </div>
   );
 }
---
+++ src/components/TaskForm/index.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "../Button";
+
+class TaskForm extends React.Component {
+    render() {
+        return (
+            <form>
+                <div>
+                    <label htmlFor="task">
+                        Add new task
+                    </label>
+                    <input
+                        type="text"
+                        name="task"
+                        id="task"
+                        placeholder="What do you want to study"
+                        required
+                    />
+
+                </div>
+
+                <div>
+                    <label htmlFor="time">
+                        Time
+                    </label>
+                    <input
+                        type="time"
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                    />
+                </div>
+                <Button/>
+            </form>
+        )
+    }
+}
+
+export default TaskForm;
